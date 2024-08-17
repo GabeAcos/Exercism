c412b57c8cfc41ec3fce339bdc2ca46b792fcf80
GabeAcos <gabrielacos143@gmail.com>
2024-08-16 19:38:04 -0500

    Adding my solution to the binary search excersise

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,2 +1,23 @@
 def find(search_list, value):
-    pass
+
+    if len (search_list) == 0:
+        raise ValueError("value not in array")
+
+    low = search_list[0]
+    high = search_list[-1]
+    while low <= high:
+        mid = (search_list.index(low) + search_list.index(high)) // 2
+        if search_list[mid] == value:
+            return mid
+        elif low == high:
+            break
+        elif search_list[mid] < value:
+            low = search_list[mid+1]
+        elif search_list[mid] > value:
+            high = search_list[mid]
+
+    raise ValueError("value not in array")
+
+
+
+
